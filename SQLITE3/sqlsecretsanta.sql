b184dfedca032a63bf93e747dec640b21e53ac4c
edogood <edoardobuono21@gmail.com>
2024-02-06 14:59:55 +0100

    updated readme, created secretsanta and sqlsecretsanta

---
+++ SQLITE3/sqlsecretsanta.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users(
+                    user_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    preferences TEXT NOT NULL,
+                    budget INTEGER NOT NULL);
+
