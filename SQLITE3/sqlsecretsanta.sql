78a13f9f4411b0694b44c94b497b71bc9dc9f061
edogood <edoardobuono21@gmail.com>
2024-02-06 17:06:42 +0100

    deleted

--- SQLITE3/sqlsecretsanta.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users(
-                    user_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                    name TEXT NOT NULL,
-                    preferences TEXT NOT NULL,
-                    budget INTEGER NOT NULL);
-
